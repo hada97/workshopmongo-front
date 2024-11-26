eaf67841e5dd906d55349f775b269a16edc3aed0
Adailson Guimaraes <99360493+hada97@users.noreply.github.com>
2024-11-26 11:31:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const SERVER_URL = "http://localhost:8080";
+const SERVER_URL = "https://workshop2-euctepgkf7hnc5fm.canadacentral-01.azurewebsites.net/";
 //const SERVER_URL = "http://localhost:8080";
 
 // Função para enviar um novo post (Create)
